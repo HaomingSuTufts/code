87bc82bc5111f57e9fd4977573fa6926cf751b88
HaomingSuTufts <haoming.su@tufts.edu>
2024-10-21 16:46:59 -0400

    L2

--- DMPNN/train.py
+++ DMPNN/train.py
@@ -209,7 +209,7 @@ class predict_model(pl.LightningModule):
 
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-5)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-5, weight_decay=1e-6)
         return optimizer
     
     
